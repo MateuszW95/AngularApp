11776c4236d01aa6ce53b141bffeb2417cdb134b
Mateusz Wieczorek <mwieczorek@i2asolutions.com>
2020-06-03 14:30:31 +0200

    Adding actors

--- server.js
+++ server.js
@@ -5,27 +5,6 @@ const cors = require("cors");
 
 const app = express();
 
-// add this code
-const whitelist = ['http://localhost:3000']; // list of allow domain
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin) {
-      return callback(null, true);
-    }
-
-    if (whitelist.indexOf(origin) === -1) {
-      var msg = 'The CORS policy for this site does not ' +
-        'allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
-}
-
-// end
-app.use(cors(corsOptions));
-
 app.use(cors());
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/AngularApp'));
