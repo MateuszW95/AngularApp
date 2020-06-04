4c7fd41b3e2f9703ed3445996b1359ad387b522b
Mateusz Wieczorek <mwieczorek@i2asolutions.com>
2020-06-04 20:20:19 +0200

    Update actors

--- src/app/mock-actors.ts
+++ src/app/mock-actors.ts
@@ -10,11 +10,20 @@ export const ACTORS: Actor[] = [
     ]
   },
   {
-    id: 37014, name: 'Marek Nowak',
+    id: 37015, name: 'Marek Nowak',
     movies: [
       {title: 'Ogniem i mieczem', value: 4.0},
       {title: 'Pan Tadeusz', value: 3.5},
       {title: 'Krzyżacy', value: 5.0},
+    ]
+  },
+  {
+    id: 37015, name: 'Michał Kowalczyk',
+    movies: [
+      {title: 'Ogniem i mieczem', value: 7.0},
+      {title: 'Pan Tadeusz', value: 2.5},
+      {title: 'Krzyżacy', value: 7.0},
+      {title: 'Pan Wołodyjowski', value: 5.0}
     ]
   }
 
